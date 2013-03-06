6f3b085ac9ad4eb5608d6d9092b56c243d283ec0
krispo <konstantin.skipor@gmail.com>
2013-03-06 16:12:04 +0400

    fixed evolutions with postgre

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,17 +2,16 @@
 
 # --- !Ups
 
--- table declarations :
 create table "user" (
     "name" 		varchar(255) not null primary key,
     "password" 	varchar(255) not null
   );
 
--- create sequence "s_user_name";
+create sequence "s_user_name";
 
 
 # --- !Downs
 
 drop table if exists  user		 CASCADE;
 
--- drop sequence if exists  s_user_name    ;
+drop sequence if exists  s_user_name    ;
